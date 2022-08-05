bbaefff255ee29b26d15a851c75b2ad1ff7e4f5e
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-08-05 16:31:01 -0600

    moved block

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const grid = document.querySelector('.grid'); 
-const block = document.createElement('div');
-block.classList.add('block');
-grid.appendChild(block);
+
+
+function addBlock() {
+
+    const block = document.createElement('div');
+    block.classList.add('block');
+    block.style.left = '100px';
+    block.style.bottom = '50px';
+    grid.appendChild(block);
+}
+addBlock();
