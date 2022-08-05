44e7df58530bb7bd31d592eaab645ac957098d7c
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-08-05 15:44:30 -0600

    it works!

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const grid = document.querySelector('grid'); 
+const grid = document.querySelector('.grid'); 
 const block = document.createElement('div');
 block.classList.add('block');
 grid.appendChild(block);
